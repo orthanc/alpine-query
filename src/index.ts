bb1243fbdb922389abc9aec60f14a4733bd02e63
Ed Costello <edc.open@gmail.com>
2024-02-26 21:13:34 +1300

    get and post magic + configurable query client and wretch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { QueryObserverResult, notifyManager } from '@tanstack/query-core';
+import {
+  QueryClientConfig,
+  QueryObserverResult,
+  notifyManager,
+} from '@tanstack/query-core';
 import {
   QueryClient,
   QueryKey,
@@ -7,15 +11,13 @@ import {
 } from '@tanstack/query-core';
 import type { Alpine } from 'alpinejs';
 import w from 'wretch';
+import type { Wretch } from 'wretch';
 
 declare global {
   interface Window {
     Alpine: Alpine;
   }
 }
-
-const queryClient = new QueryClient();
-const wretch = w();
 
 const query = <
   TQueryFnData,
@@ -24,6 +26,7 @@ const query = <
   TQueryData,
   TQueryKey extends QueryKey,
 >(
+  queryClient: QueryClient,
   options: QueryObserverOptions<
     TQueryFnData,
     TError,
@@ -73,31 +76,82 @@ const query = <
 };
 
 document.addEventListener('alpine:init', () => {
+  window.Alpine.magic(
+    'queryClient',
+    () => (config?: QueryClientConfig) => new QueryClient(config)
+  );
+  window.Alpine.magic(
+    'wretch',
+    () => (baseUrl?: string, options?: RequestInit) => w(baseUrl, options)
+  );
+  window.Alpine.magic('get', (_, { evaluate }) => {
+    const wretch = evaluate('wretch') as Wretch;
+    return (
+      url: string,
+      fetchOptions?: RequestInit,
+      queryOptions?: Partial<QueryObserverOptions>
+    ): QueryObserverOptions => ({
+      queryFn: () => {
+        const obj = wretch
+          .options(fetchOptions ?? {})
+          .get(url)
+          .json();
+        return obj;
+      },
+      queryKey: [url],
+      ...queryOptions,
+    });
+  });
+  window.Alpine.magic('post', (_, { evaluate }) => {
+    const wretch = evaluate('wretch') as Wretch;
+    return (
+      url: string,
+      body: any,
+      fetchOptions?: RequestInit,
+      queryOptions?: Partial<QueryObserverOptions>
+    ): QueryObserverOptions => ({
+      queryFn: () => {
+        let chain = wretch.options(fetchOptions ?? {}).url(url);
+        if (body !== undefined) {
+          chain = chain.json(body);
+        }
+        return chain.post().text();
+      },
+      queryKey: [url],
+      ...queryOptions,
+    });
+  });
   window.Alpine.directive(
     'query',
     (
       el,
       { value: variable = 'query', expression },
-      { Alpine, evaluateLater, effect, cleanup }
+      { Alpine, evaluate, evaluateLater, effect, cleanup }
     ) => {
+      const queryClient = Alpine.raw(evaluate('queryClient')) as QueryClient;
+      if (queryClient == null) {
+        throw new Error(
+          'No query client is set, you must have x-data="{queryClient: $queryClient()}" as a parent before use of x-query'
+        );
+      }
       const getQuery = evaluateLater(expression);
 
       let updateOptions: undefined | ((options: QueryObserverOptions) => void) =
         undefined;
       const updateResult = (result: QueryObserverResult) => {
-        // @ts-expect-error
+        // @ts-expect-error alpine data doesn't seem like something I can type
         Alpine.$data(el)[variable] = result;
       };
 
       effect(() =>
-        getQuery((url: string) => {
-          const options: QueryObserverOptions = {
-            queryFn: () => wretch.get(url).json(),
-            queryKey: [url],
-          };
+        getQuery((options: QueryObserverOptions) => {
           if (updateOptions == null) {
             let unsubscribe: () => void;
-            ({ unsubscribe, updateOptions } = query(options, updateResult));
+            ({ unsubscribe, updateOptions } = query(
+              queryClient,
+              options,
+              updateResult
+            ));
             cleanup(unsubscribe);
           } else {
             updateOptions(options);
